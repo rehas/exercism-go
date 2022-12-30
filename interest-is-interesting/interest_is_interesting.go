0932b306f359b7a2ad338c73f28c29ae11a2d972
RehaS <beratson@gmail.com>
2022-12-30 18:10:38 +0100

    go track exercise: interest-is-interesting

---
+++ interest-is-interesting/interest_is_interesting.go
@@ -0,0 +1,38 @@
+package interest
+
+// InterestRate returns the interest rate for the provided balance.
+func InterestRate(balance float64) float32 {
+	switch {
+        case balance < 0.0:
+    		return 3.213
+        case balance < 1000.0:
+    		return 0.5
+        case balance < 5000.0:
+    		return 1.621
+        default:
+    		return 2.475
+    }
+}
+
+// Interest calculates the interest for the provided balance.
+func Interest(balance float64) float64 {
+	return balance * float64(InterestRate(balance) / 100.0)
+}
+
+// AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
+func AnnualBalanceUpdate(balance float64) float64 {
+	return Interest(balance) + balance
+}
+
+// YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance:
+func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
+	var years int
+    for{
+        if balance >= targetBalance{
+            break
+        }
+    	balance = AnnualBalanceUpdate(balance)
+        years++
+    }
+    return years
+}
